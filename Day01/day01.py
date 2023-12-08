648e501bed0a1c1e4de2458ca07e30ec34d1d7ab
Thomas Wilde <thomas@isg.cs.ovgu.de>
2023-12-08 08:11:05 +0100

    CHANGE: read in puzzle data line by line

--- Day01/day01.py
+++ Day01/day01.py
@@ -1,1 +1,10 @@
-print("test")
+print("Day 1 - Trebuchet")
+
+# Using readlines()
+# file = open("input.data", "r")
+file = open("example.data", "r")
+lines = file.readlines()
+
+# read lines
+for line in lines:
+    print(line)
