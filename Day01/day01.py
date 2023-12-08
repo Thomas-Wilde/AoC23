8f265314e3e664c2285e5abf4ead8354a0280820
Thomas Wilde <thomas@isg.cs.ovgu.de>
2023-12-08 08:40:06 +0100

    CHANGE: added code to extract second digit (again)

--- Day01/day01.py
+++ Day01/day01.py
@@ -8,8 +8,27 @@ lines = file.readlines()
 # read lines
 for line in lines:
     print(line)
+    d0 = "0"
+    d1 = "0"
+    # ---
     for i in range(len(line)):
         c = line[i]
         if c.isnumeric():
-            print(c)
+            d0 = int(c)
+            # print(int(c))
             break
+    # ---
+    for index in reversed(range(len(line))):
+        character = line[index]
+        # --- check if it is a numeric
+        if character.isnumeric():
+            # print(c)
+            d1 = int(character)
+            # --- we do not need to got further
+            break
+    # ---
+    print(d0)
+    print(d1)
+    number = d0 * 10 + d1
+    # number = "d0" + "d1"
+    print(number)
