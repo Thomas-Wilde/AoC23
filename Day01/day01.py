de90cdd06446eb69a4355f077a5861cc7a18ac64
Thomas Wilde <thomas@isg.cs.ovgu.de>
2023-12-08 08:26:59 +0100

    CHANGE: extract second digit from

--- Day01/day01.py
+++ Day01/day01.py
@@ -8,8 +8,15 @@ lines = file.readlines()
 # read lines
 for line in lines:
     print(line)
+    # extract first digit
     for i in range(len(line)):
         c = line[i]
+        if c.isnumeric():
+            print(c)
+            break
+    # extract second digit
+    for j in range(len(line) - 1, 0, -1):
+        c = line[j]
         if c.isnumeric():
             print(c)
             break
